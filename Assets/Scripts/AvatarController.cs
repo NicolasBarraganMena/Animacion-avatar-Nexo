50652a6f4910e04992a0e0e9fc901e7b6d81beb8
Nicolas Barragan <nicolas.barragan@upb.edu.co>
2021-05-13 11:18:00 -0500

    Animator Controller Walk 4

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -9,6 +9,7 @@ public class AvatarController : MonoBehaviour
     public float velocidadRotacion;
     private Animator anim;
     public float x, y;
+    private bool estaBailando = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +17,17 @@ public class AvatarController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+    }
+
     // Update is called once per frame
     void Update()
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-
-        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
 
         anim.SetFloat("Vel(x)", x);
         anim.SetFloat("Vel(y)", y);
